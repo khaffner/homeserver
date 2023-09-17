04349895a3588907670e3f555f02441a310553d9
Kevin Haffner <khaffner@gmail.com>
2023-09-17 11:58:01 +0200

    Update script.sh

--- powermeter/script.sh
+++ powermeter/script.sh
@@ -6,7 +6,7 @@ while true; do
     LINE="${LINE// /_}"
 
     # Print the line for loggings sake
-    echo $LINE
+    # echo $LINE
 
     # Publish value to mqtt server
     mosquitto_pub -h 192.168.1.8 -t powermeter -m $LINE -u $MQTTUSER -P $MQTTPASS
